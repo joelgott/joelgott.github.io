683b727e89fff3677786d88ab8969c2487756cda
jgottfried <jgottfried@tesacom.net>
2025-02-28 15:56:54 -0300

    cv update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,6 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/joelgottfriedCV.pdf";
+import pdf_en from "../../Assets/../Assets/joelgottfriedCVEN.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -24,29 +25,25 @@ function ResumeNew() {
             variant="primary"
             href={pdf}
             target="_blank"
-            style={{ maxWidth: "250px" }}
+            style={{ maxWidth: "250px", marginRight: "10px"}}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download CV (Spanish)
           </Button>
-        </Row>
-
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdf_en}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download CV (English)
           </Button>
+        </Row>
+        <Row className="resume">
+          <Document file={pdf_en} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
       </Container>
     </div>
